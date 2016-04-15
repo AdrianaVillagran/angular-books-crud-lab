09780b5f8051ceca831d3e07a02135df4d33aa43
AdrianaVillagran <anvillagran1@gmail.com>
2016-04-15 14:29:37 -0700

    added create function

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -5,6 +5,7 @@ BooksIndexController.$inject=['$http'];
 function BooksIndexController( $http ) {
   var vm = this;
   var endpoint = "https://super-crud.herokuapp.com/books";
+  vm.newBook = {};
 
   $http({
     method: 'GET',
@@ -15,4 +16,18 @@ function BooksIndexController( $http ) {
   }, function errorCallback(response) {
     console.log('error getting books', response);
   });
+
+  vm.createBook = function () {
+    $http({
+      method: 'POST',
+      url: endpoint,
+      data: vm.newBook
+    }).then(function successCreateCallback(response) {
+      console.log(response.data);
+      vm.newBook = response.data;
+      vm.books.push(vm.newBook);
+    }, function errorCreateCallback(response) {
+      console.log('error creating book', err);
+    } );
+  };
 }
