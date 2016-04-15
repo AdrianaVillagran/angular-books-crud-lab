49ef226223cbacb8508d99f7835cf4b627bd4181
AdrianaVillagran <anvillagran1@gmail.com>
2016-04-15 12:09:27 -0700

    added get call on BooksIndexController

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -4,4 +4,14 @@ angular.module('libraryApp')
 BooksIndexController.$inject=['$http'];
 function BooksIndexController( $http ) {
   var vm = this;
+  var endpoint = "https://super-crud.herokuapp.com/books";
+
+  $http({
+    method: 'GET',
+    url: endpoint
+  }).then(function successCallback(response) {
+    console.log(response.data);
+  }, function errorCallback(response) {
+    console.log('error getting books', response);
+  });
 }
